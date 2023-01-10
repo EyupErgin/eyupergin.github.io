9df949d2707ad576219ae051fc9ed614f2681392
Eyup Sukru ERGIN <ergindev@gmail.com>
2023-01-10 22:49:14 +0300

    Add files via upload

---
+++ elena/searchscript.js
@@ -0,0 +1,235 @@
+function ALL(){
+	IntelX();
+    	Tor();
+	Dehashed();
+	Genesis();
+	Russian();
+	Turkish();
+	Global();
+	MxDomain();
+	Github();
+	Ransomware();
+	//Alllookup();
+}
+
+// FORUMLAR ICIN KULLANILAN MODUL!
+function Global(){
+    Breached();
+    ExploitIn();
+    XssIs();
+    CracKing();
+    SinisterLy();
+    CrackXPro();
+    SinfullSite();
+    CrackedIO();
+    ClubHydra();
+    NulledTo()
+}
+
+function Turkish(){
+    ImhaTimi();
+    SpyHackerz();
+    Siberdeyiz();
+    Hacktivizm();
+    CrackTurkey();
+    Koswog();
+}
+
+function AllBlacklist(){
+    MxDomain();
+}
+
+function Alllookup(){
+    MxDmarc();
+    Mxmxlookup();
+    MxDns()
+}
+
+function Tor(){
+    Ahmia();
+}
+
+
+function Breached(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://breached.vc/search.php?action=do_search&keywords="+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+function ExploitIn(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://forum.exploit.in/search/?q="+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+function XssIs(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://xss.is/search/search/?keywords="+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+function ImhaTimi(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://imthack.com/forum/search/search/?keywords="+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+function Hacktivizm(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://www.hacktivizm.org/search/search/?keywords="+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+function CrackTurkey(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://crackturkey.com/search/search/?keywords="+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+function SpyHackerz(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://spy.gen.tr/forum/search/search/?keywords="+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+function Siberdeyiz(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://siberdeyiz.com/search/search/?keywords="+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+function Koswog(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://forum.koswog.com/search/search/?keywords="+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+function CracKing(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://cracking.org/search/search/?keywords="+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+function SinisterLy(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://sinister.ly/search.php?action=do_search&keywords="+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+function CrackXPro(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://craxpro.io/google-search/?q="+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+
+function SinfullSite(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://sinfulsite.com/search.php?action=do_search&keywords="+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+
+function CrackedIO(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://cracked.io/search.php?action=do_search&keywords="+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+
+function ClubHydra(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://www.clubhydra.co/search.php?action=do_search&keywords="+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+
+function NulledTo(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://www.nulled.to/index.php?app=core&module=search&section=search&do=search&fromsearch=1&search_term="+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+
+// BLACKLIST ICIN KULLANILAN MODUL!
+function MxDomain(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://mxtoolbox.com/SuperTool.aspx?action=blacklist%3a"+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+
+
+// BLACKMARKET ICIN KULLANILAN MODUL!
+function Genesis(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://genesis.market/client/bots/index?BotsSearch%5BallResources%5D="+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+function Russian(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://russianmarket.to/logs?links="+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+
+
+// MAIL ICIN KULLANILAN MODUL!
+function IntelX(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://intelx.io/?s="+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+function Dehashed(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://www.dehashed.com/search?query=domain%3A"+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+
+
+// LOOKUP ICIN KULLANILAN MODUL!
+function MxDmarc(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://mxtoolbox.com/SuperTool.aspx?action=dmarc%3a"+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+function Mxmxlookup(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://mxtoolbox.com/SuperTool.aspx?action=mx%3a"+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+function MxDns(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://mxtoolbox.com/SuperTool.aspx?action=a%3a"+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+
+
+// RANSOMWARE ICIN KULLANILAN MODUL!
+function Ransomware(){
+    var url = "https://privtools.github.io/ransomposts/";
+    window.open(url);
+}
+
+// CODE LEAK ICIN KULLANILAN MODUL!
+function Github(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://github.com/search?q="+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+
+// AHMIA ICIN KULLANILAN MODUL!
+function Ahmia(){
+    var query  = document.myform.query.value;
+    var finalQuery= query+"";
+    var url = "https://ahmia.fi/search/?q="+encodeURIComponent(finalQuery);
+    window.open(url);
+}
+
